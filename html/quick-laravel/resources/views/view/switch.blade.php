4127e51d994bb36c72158178baaf6b11414bd758
Yuzunoha <studysteadystandby@gmail.com>
2021-03-05 19:39:14 +0900

    うらないできた

--- html/quick-laravel/resources/views/view/switch.blade.php
+++ html/quick-laravel/resources/views/view/switch.blade.php
@@ -1,2 +1,26 @@
-switchswitchswitch
 {{ $num }}
+
+@switch($num)
+    @case(1)
+    わるい
+    @break
+
+    @case(2)
+    ややわるい
+    @break
+
+    @case(3)
+    ふつう
+    @break
+
+    @case(4)
+    ややよい
+    @break
+
+    @case(5)
+    よい
+    @break
+
+    @default
+    不明
+@endswitch
